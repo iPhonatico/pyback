766109aa63929bc7bfd7e306a2c58b1665f33152
iPhonatico <49172467+iPhonatico@users.noreply.github.com>
2024-09-08 10:40:05 -0500

    se añadio el readme paara probar reservaciones

--- organization/models.py
+++ organization/models.py
@@ -5,21 +5,15 @@ from django.core.exceptions import ValidationError
 class Parking(models.Model):
     name = models.CharField(max_length=100)
     address = models.TextField()
-    capacity = models.PositiveIntegerField(default=0)
-    actualCapacity = models.PositiveIntegerField()  # Capacidad actual del parqueo
+    capacity = models.PositiveIntegerField(default=0)  # Capacidad total del parqueo
     fee = models.DecimalField(decimal_places=2, max_digits=5)
     user = models.ForeignKey('authentication.User', on_delete=models.CASCADE, null=True, blank=True)
     available = models.BooleanField(default=True)
 
-    def save(self, *args, **kwargs):
-        # Al crear un nuevo parqueo, inicializar actualCapacity con la capacidad total si está vacío
-        if not self.pk or self.actualCapacity == 0:  # Si es un parqueo nuevo o la capacidad actual es 0
-            self.actualCapacity = self.capacity
-
-        super().save(*args, **kwargs)
-
     def __str__(self):
-        return f'{self.name} - {self.address} ({self.actualCapacity}/{self.capacity} disponibles)'
+        return f'{self.name} - {self.address} (Capacidad: {self.capacity})'
+
+
 
 
 
@@ -40,7 +34,14 @@ class ParkingSchedule(models.Model):
     date = models.DateField()
     parking = models.ForeignKey('organization.Parking', on_delete=models.CASCADE)
     schedule = models.ForeignKey('organization.Schedule', on_delete=models.CASCADE)
-    #available = models.BooleanField(default=True)  # Puedes eliminar este campo si ya no es necesario
+    actualCapacity = models.PositiveIntegerField(default=0)  # Capacidad actual para este horario específico
+
+    def save(self, *args, **kwargs):
+        # Al crear un nuevo horario, inicializar actualCapacity con la capacidad total del parqueo
+        if not self.pk or self.actualCapacity == 0:
+            self.actualCapacity = self.parking.capacity
+
+        super().save(*args, **kwargs)
 
     def clean(self):
         overlapping_schedule = ParkingSchedule.objects.filter(
