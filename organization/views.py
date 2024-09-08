0bf673239d3138e7eb487c37e2cd2fb5e7db6fdb
iPhonatico <49172467+iPhonatico@users.noreply.github.com>
2024-09-08 13:37:10 -0500

    ya funcionan las reservas, solo falta la automática

--- organization/views.py
+++ organization/views.py
@@ -8,20 +8,50 @@ from .models import Parking
 from rest_framework import permissions, viewsets
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import permissions, viewsets, filters
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .serializers import *
 from rest_framework.permissions import IsAuthenticated, DjangoModelPermissions
 from django.http import JsonResponse
 from .filters import ParkingFilter
-
+from rest_framework import status
 
 class ParkingViewSet(viewsets.ModelViewSet):
     queryset = Parking.objects.all()
     serializer_class = ParkingSerializer
     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
-    filterset_class = ParkingFilter  # Asignar el filtro personalizado
+    filterset_class = ParkingFilter
     search_fields = ['name']
-    #permission_classes = [IsAuthenticated, DjangoModelPermissions]
+
+    @action(detail=True, methods=['get'])
+    def available_schedules(self, request, pk=None):
+        """
+        Devuelve los horarios disponibles para un parqueo específico con capacidad restante.
+        """
+        try:
+            parking = self.get_object()
+            available_schedules = ParkingSchedule.objects.filter(parking=parking, actualCapacity__gt=0)
+
+            if not available_schedules.exists():
+                return Response({"detail": "No hay horarios disponibles para este parqueo."}, status=status.HTTP_404_NOT_FOUND)
+
+            # Crear respuesta con horarios disponibles
+            result = []
+            for schedule in available_schedules:
+                result.append({
+                    "date": schedule.date,
+                    "schedule": {
+                        "start_time": schedule.schedule.start_time,
+                        "end_time": schedule.schedule.end_time,
+                    },
+                    "available_capacity": schedule.actualCapacity
+                })
+
+            return Response(result, status=status.HTTP_200_OK)
+
+        except Parking.DoesNotExist:
+            return Response({"detail": "El parqueo no existe."}, status=status.HTTP_404_NOT_FOUND)
 
 
 class ScheduleViewSet(viewsets.ModelViewSet):
