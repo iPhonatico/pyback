5f5ad47a6da4fdb010ff7ed63ea41175603cac88
iPhonatico <49172467+iPhonatico@users.noreply.github.com>
2024-09-08 03:51:29 -0500

    se actualizó git ignore para todos los __pycache__

--- organization/models.py
+++ organization/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.core.exceptions import ValidationError
 # Create your models here.
 
 class Parking(models.Model):
@@ -9,12 +9,21 @@ class Parking(models.Model):
     fee = models.DecimalField(decimal_places=2, max_digits=5)
     user = models.ForeignKey('authentication.User', on_delete=models.CASCADE, null=True, blank=True)
     available = models.BooleanField(default=True)
-    def __str__(self):  #para que aparezca el nombre en el admin
-        return self.name
+    def __str__(self):
+        return f'{self.name} - {self.address} ({self.capacity} slots)'
 
 class Schedule(models.Model):
     start_time = models.TimeField()
     end_time = models.TimeField()
+
+    def clean(self):
+        if self.start_time >= self.end_time:
+            raise ValidationError('La hora de inicio debe ser antes de la hora de fin')
+
+    def __str__(self):
+        return f'{self.start_time} - {self.end_time}'
+
+
 
     def __str__(self):
         return str(self.start_time) + " - " + str(self.end_time)
